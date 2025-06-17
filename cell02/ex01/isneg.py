8d09c8ede94648e55e8756621bb5c21040370c6d
Phummi888 <poomzaza0122@gmail.com>
2025-06-17 11:54:29 +0000

    01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+x = int(input("enter a number"))
+if x > 0:
+    print("This number is positive")
+if x < 0:
+    print("This number is negative")
+if x == 0:
+    print("This number is both positive and negative.")
