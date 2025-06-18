5dbe08f294159e1ce9429b7497104d60b2301fe5
Phummi888 <poomzaza0122@gmail.com>
2025-06-18 08:50:48 +0000

    04

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    # ไม่รวมชื่อไฟล์ (sys.argv[0])
+    num_params = len(sys.argv) - 1
+    print(f"Number of parameters: {num_params}.")
+
+if name == "main":
+    main()
