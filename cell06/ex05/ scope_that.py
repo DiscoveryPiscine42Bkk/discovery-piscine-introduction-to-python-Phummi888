5564e39130ee48333c8205eead74168276efadde
Phummi888 <poomzaza0122@gmail.com>
2025-06-19 08:01:00 +0000

    05

---
+++ cell06/ex05/ scope_that.py
@@ -0,0 +1,7 @@
+def add_customer(value):
+    return value + 1
+if __name__ == "__main__":
+    my_var = 5
+    print("Before calling add_one:", my_var)
+    my_var = add_one(my_var)
+    print("After calling add_one:", my_var)
